54d9c70d1c5f61e7b8623914e656c378b7345ca0
marlynhenry <marlynhenry2@gmail.com>
2015-10-09 10:41:29 -0400

    changes
    changes made

--- CS_Chapter2/DoubleDecimalTest/Program.cs
+++ CS_Chapter2/DoubleDecimalTest/Program.cs
@@ -9,6 +9,9 @@ namespace DoubleDecimalTest
     {
         static void Main(string[] args)
         {
+
+            double = 39;
+            decimal = 7000.463781636481634916396916381563173547851634M;
         }
     }
 }

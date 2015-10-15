d690135c88cf2d121f11196c4ac3857029d5b520
marlynhenry <marlynhenry2@gmail.com>
2015-10-15 10:52:20 -0400

    yasss we done

--- CS_Chapter2/EggsInteractive/Program.cs
+++ CS_Chapter2/EggsInteractive/Program.cs
@@ -9,7 +9,14 @@ namespace EggsInteractive
     {
         static void Main(string[] args)
         {
-           
+            double me1 = Convert.ToDouble(Console.ReadLine());
+            double me2 = Convert.ToDouble(Console.ReadLine());
+            double me3 = Convert.ToDouble(Console.ReadLine());
+            double me4 = Convert.ToDouble(Console.ReadLine());
+            double total = me1 + me2 + me3 + me4;
+            double dozen = total / 12;
+            double eggs = total % 12;
+            Console.WriteLine(" A total of {0} eggs is equal {1} dozen and {2} eggs", total, dozen, eggs);
         }
     }
 }

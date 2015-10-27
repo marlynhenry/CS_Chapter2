2999a731c58bf7a5593b797006795f39a40a5948
marlynhenry <marlynhenry2@gmail.com>
2015-10-27 10:34:44 -0400

    we made it again, hopefully

--- CS_Chapter2/DoubleDecimalTest/Program.cs
+++ CS_Chapter2/DoubleDecimalTest/Program.cs
@@ -10,7 +10,7 @@ namespace DoubleDecimalTest
         static void Main(string[] args)
         {
 
-            //double me = 39;
+            double me = 39;
             //decimal me2 = 7000.463781636481634916396916381563173547851634M;
         }
     }
